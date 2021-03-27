394e24b12824eee1057b9ad4ee01d815ec403a87
unknown <77127752+leahgraced@users.noreply.github.com>
2021-03-27 15:17:40 -0600

    Updating PyPoll; variables/lists declared

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,23 @@ import os
 #State working data location
 csvpath=os.path.join("Resources", "election_data.csv")
 
+# Open csv
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    cdf
+    # Skip the header to grab data
+    csv_header = next(csvreader)
+
+    #Declare lists
+    voter_id = []
+    county = []
+    name = []
+    khan_votes = []
+    correy_votes = []
+    li_votes =[]
+    otooley_votes = []
+
+    # Loop through the data
+    for row in csvreader:
+        voter_id.append(row[0])
+        county.append(row[1])
+        name.append(row[2])
